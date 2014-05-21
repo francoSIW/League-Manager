193a04b2efa671f5322ef2ba329a2ebd41e38edb
Franco Piperno <francopiperno@live.it>
2014-05-21 12:14:22 +0200

    dfghj

--- League_Manager/src/league/model/Main.java
+++ League_Manager/src/league/model/Main.java
@@ -6,7 +6,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-
+//XGVHJBKNLM
 public class Main {
 	
 	public static void main(String[] args){
